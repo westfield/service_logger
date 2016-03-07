148a288aa540b8643deb07e652c22b6c28fefdd8
Kisha Richardson <kisha@spunklabs.com>
2016-03-07 13:44:37 -0800

    [WFAPI-1848] Structured Logging Gem Phase 1

--- lib/service_logger.rb
+++ lib/service_logger.rb
@@ -1,5 +1,16 @@
-require "service_logger/version"
+require 'service_logger/version'
+require 'active_support/concern'
 
 module ServiceLogger
-  # Your code goes here...
+  extend ActiveSupport::Concern
+  included do
+    private
+
+    def service_log(info, service_message, service_details, tag_name="none")
+      logger.tagged(tag_name, "#{Time.now.utc}") do
+        message = { service_name: info[:name], environment: info[:environment], service_message: service_message, service_details: service_details }
+        logger.info(message.to_json)
+      end
+    end
+  end
 end
